b8b09ef89eb96e62d078faca379c9864da26498f
NatSama2 <vet.nat.c@gmail.com>
2024-01-19 14:50:23 -0300

    creando los archivos y la bienvenida al juego

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -3,7 +3,12 @@ package src;
 public class GuessTheNumberGame {
   
   public static void main(String[] args) {
-    System.out.println("Hola Mundo");
+    System.out.println("******************************************************");
+    System.out.println("*                                                    *");
+    System.out.println("*               Welcome to GuessTheNumber            *");
+    System.out.println("*                       Game                         *");
+    System.out.println("*                                                    *");
+    System.out.println("******************************************************");
 
     // javac src/GuessTheNumberGame.java
     // java -cp . src.GuessTheNumberGame 
