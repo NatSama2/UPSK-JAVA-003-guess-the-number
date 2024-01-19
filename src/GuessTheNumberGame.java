ab77f96bf95beb185633a2514676354f7e09afa5
NatSama2 <vet.nat.c@gmail.com>
2024-01-18 23:22:32 -0300

    commit

---
+++ src/GuessTheNumberGame.java
@@ -0,0 +1,12 @@
+package src; 
+
+public class GuessTheNumberGame {
+  
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+
+    // javac src/GuessTheNumberGame.java
+    // java -cp . src.GuessTheNumberGame 
+  }
+
+}
