29d850b971c8f463a54c4d72254aea932c9f7244
NatSama2 <vet.nat.c@gmail.com>
2024-01-22 22:37:04 -0300

    probando randomNumber

--- src/GuessTheNumberGame.java
+++ src/GuessTheNumberGame.java
@@ -1,17 +1,49 @@
-package src; 
+package src;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class GuessTheNumberGame {
   
-  public static void main(String[] args) {
-    System.out.println("******************************************************");
-    System.out.println("*                                                    *");
-    System.out.println("*               Welcome to GuessTheNumber            *");
-    System.out.println("*                       Game                         *");
-    System.out.println("*                                                    *");
-    System.out.println("******************************************************");
+  public static final String RED_COLOR = "\u001B[31m";
+  public static final String RESET_COLOR = "\u001B[0m";
 
+  public static int generateRandomNumber() {
+    Random random = new Random();
+      return random.nextInt(100) +1;
+  }
+
+  public static void main(String[] args) {
+
+    System.out.println(" " + RED_COLOR);
+    System.out.println("******************************************************" + RED_COLOR);
+    System.out.println("*                                                    *" + RED_COLOR);
+    System.out.println("*               Welcome to GuessTheNumber            *" + RED_COLOR);
+    System.out.println("*                       Game                         *" + RED_COLOR);
+    System.out.println("*                                                    *" + RED_COLOR);
+    System.out.println("******************************************************" + RED_COLOR);
+
+    System.out.print(RESET_COLOR);
+    
     // javac src/GuessTheNumberGame.java
     // java -cp . src.GuessTheNumberGame 
+
+    //Se muestra el número random generado
+    int randomNumber = generateRandomNumber();
+    System.out.println("Random number generated: " + randomNumber);
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a number between 1 and 100: ");
+    int userGuess = scanner.nextInt();
+
+    System.out.println("You entered: " + userGuess);
+
+    scanner.close();
+
+    HumanPlayer humanPlayer = new HumanPlayer();
+    ComputerPlayer computerPlayer = new ComputerPlayer();
+    
   }
 
 }
